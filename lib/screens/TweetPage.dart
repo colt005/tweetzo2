fa972c5065a81486ee08365306ab6a267d301358
colt005 <rohan.prabhu05@gmail.com>
2019-06-28 19:09:08 +0530

    Tweets Working

--- lib/screens/TweetPage.dart
+++ lib/screens/TweetPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 import 'dart:async';
 import 'dart:convert';
@@ -8,11 +9,15 @@ import '../Keys/secrets.dart';
 import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 import 'package:flutter_share_me/flutter_share_me.dart';
 
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
+
 String SearchUrl = "/search/tweets.json?q=";
-String SearchQuery = "filter%3Averified%20filter%3Anews";  //filter%3Averified%20filter%3Anews
+String SearchQuery = 'filter%3Averified%20filter%3Anews&geocode=$userLattitude,$userLongitude,300km&result_type=mixed&include_entities=true&lang=en';  //filter%3Averified%20filter%3Anews
 String SearchLat = "";
 String SearchLong = "";
 String SearchRad = "";
+String userLattitude;
+String userLongitude;
 Map data;
 List tweetStatus;
 Map tweetUser;
@@ -31,26 +36,34 @@ class TweetPage extends StatefulWidget{
        @override
   void initState() {
     super.initState();
-    getData();
+    getLocation();
   }
-    
+    void getLocation() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    debugPrint(position.toString());
+    userLongitude = position.longitude.toString();
+    userLattitude = position.latitude.toString();
+       
+    getData();
+    // tryonr();
+  }
 
   void getData() async{
     Twitter twitter = new Twitter(secrets().CONSUMER_KEY, secrets().CONSUMER_SECRET,
         secrets().ACCESS_TOKEN, secrets().ACCESS_TOKEN_SECRET);
     var response = await twitter.request("GET", SearchUrl+SearchQuery);
-    debugPrint(response.body.toString());
+    
     data = json.decode(response.body) as Map;
+    debugPrint("Rohan"+data.toString());
     //debugPrint("Ronnnn"+data.toString());
    // List lis2 = new List.from(data[0]);
     //debugPrint("Ronnn"+lis2.toString());
     tweetStatus = new List.from(data['statuses']);
-    for(int i=0;i<tweetStatus.length;i++){
-      tweetUser = new Map.from(data['statuses'][i]['user']);
-    }
+    
     //TODO: Get User from tweet
     //tweetUser = new Map.from(data['user']);
-    debugPrint("Ronnnn"+tweetUser.toString());
+    
 
 
     setState(() {});
@@ -76,8 +89,9 @@ class TweetPage extends StatefulWidget{
         itemBuilder: (BuildContext context, int index) {
           return Card(
             child: ListTile(
-              title: Text(tweetStatus[index]['text'].toString()),
-              
+              subtitle: Text(tweetStatus[index]['text'].toString()),
+              title: Text(tweetStatus[index]['user']['name']),
+           
               
             ),
 
